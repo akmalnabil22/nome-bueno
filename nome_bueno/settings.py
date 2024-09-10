9d7210f9424a99c9c2a3b42b284cc50af9db41e7
akmalnabil22 <akmaln761@gmail.com>
2024-09-10 07:47:03 +0700

    add allowed host

--- nome_bueno/settings.py
+++ nome_bueno/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)(h+(mczup1j-=09sw314(=zvw%-810jx-5uafdgk_g0y)rr)$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "akmal-nabil-nomebueno.pbp.cs.ui.ac.id"]
 
 
 # Application definition
